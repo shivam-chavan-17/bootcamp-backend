a3a3852b7e868e3bc8aabe9ee58586771d9bb55a
shivam-chavan-17 <vc19016@gmail.com>
2024-10-08 21:59:05 +0530

    cors

--- server.js
+++ server.js
@@ -17,6 +17,11 @@ app.get('/', (req, res) => {
 
 app.use('/api/auth', authRoutes);
 
+const cors = require('cors');
+app.use(cors({
+    origin: 'https://bootcamp-web.onrender.com', // Allow only your frontend
+}));
+
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, console.log(`Server running on port ${PORT}`));
