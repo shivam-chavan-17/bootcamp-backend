cd06be98e15c45a44b5dd112869b03eb42d6a03c
Shivam Chavan <vc19016@gmail.com>
2024-10-10 16:10:01 +0530

    server cors

--- server.js
+++ server.js
@@ -3,22 +3,19 @@ const express = require('express');
 const dotenv = require('dotenv');
 const connectDB = require('./config/db');
 const authRoutes = require('./routes/authRoutes');
-
 const cors = require('cors');
-
-const corsOptions = {
-  origin: ['https://bootcamp-web.onrender.com', 'https://localhost:3000'], // Add frontend URL
-  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
-  allowedHeaders: ['Content-Type', 'Authorization'],
-};
-
-// app.use(cors(corsOptions));
-
 
 dotenv.config();
 connectDB();
 
+const corsOptions = {
+  origin: ['https://bootcamp-web.onrender.com', 'http://localhost:3000'], // Add frontend URL
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+  allowedHeaders: ['Content-Type', 'Authorization'],
+};
+
 const app = express();
+app.use(cors(corsOptions));
 app.use(express.json());  // Body parser middleware
 
 // Simple Route to test API
@@ -27,13 +24,6 @@ app.get('/', (req, res) => {
 });
 
 app.use('/api/auth', authRoutes);
-
-// const cors = require('cors');
-// app.use(cors({
-//     origin: 'https://bootcamp-web.onrender.com', // Allow only your frontend
-// }));
-app.use(cors(corsOptions));
-
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, console.log(`Server running on port ${PORT}`));
